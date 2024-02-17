85234d2882b60b43d8710c1e8838512deeb79416
EvgenyBusleiko <busleiko@mail.ru>
2024-02-17 09:27:53 +0300

    add sem2ht1

--- sem2ht1.py
+++ sem2ht1.py
@@ -6,37 +6,38 @@ app.secret_key = b'5f214cacbd30c2ae4784b520f17912ae0d5d8c16ae98128e3f54954622126
 
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
+
     if request.method == 'POST':
         name = request.form.get('login')
-        password = request.form.get('password')
+        password = request.form.get('email')
+
+        # print(request.cookies.get('login'))
+        # print(request.cookies.get('pas'))
 
 
-
-
-
-        if name == 'admin' and password == 'password':
-            response = make_response()
-            response.set_cookie('login', value=name)
-            response.set_cookie('pas', value=password)
-            return render_template('welcome.html', context=name)
-        else:
-            flash('Ошибка логина или пароля', 'danger')
-            return redirect(url_for('submit'))
+        response = make_response(render_template('welcome.html', contex=name))
+        response.set_cookie('login', value=name)
+        response.set_cookie('pas', value=password)
+        return response
 
     return render_template('password.html')
+
+
 
 
 @app.route('/', methods=['GET', 'POST'])
 def redirect_to_inedex():
     return redirect(url_for('submit'))
+
+
 @app.route('/forward/', methods=['GET', 'POST'])
 def return_to_main():
     if request.method == 'POST':
-        response = make_response()
-        response.set_cookie('login','', max_age=0)
-        response.set_cookie('pas','', max_age=0)
-        return render_template('password.html')
-
+        response = make_response(render_template('password.html'))
+        response.set_cookie('login', max_age=0)
+        response.set_cookie('pas', max_age=0)
+        return response
+    return render_template('password.html')
 
 
 if __name__ == '__main__':
